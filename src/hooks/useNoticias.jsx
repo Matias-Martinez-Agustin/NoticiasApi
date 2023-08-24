8ffc37e637aab8a3e78336303b8ca6fcb7b99626
Matias Martinez <matiagus1997@gmail.com>
2023-08-24 05:03:55 -0300

    first commit

--- src/hooks/useNoticias.jsx
+++ src/hooks/useNoticias.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import NoticiasContext from "../context/noticiasProvider"
+import NoticiasContext from "../context/NoticiasProvider"
 
 const useNoticias = ()=> {
     return useContext(NoticiasContext)
